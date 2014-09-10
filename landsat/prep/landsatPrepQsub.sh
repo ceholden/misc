0bd5849f47dc9327e5885e7d347b1d2cb0bc1a4a
Chris Holden <ceholden@gmail.com>
2014-09-10 12:07:53 -0400

    Added batch preprocessing scripts for BU sun grid engine cluster

---
+++ landsat/prep/landsatPrepQsub.sh
@@ -0,0 +1,493 @@
+#!/bin/bash
+
+###
+# 
+# Name:     landsatPrepQsub.sh
+# Version:  3.0
+# Date:     10/27/2012
+#
+# Author:   Chris Holden (ceholden@bu.edu)
+#
+# Purpose:  For input Landsat tar.gz, extract and organize  each image by
+#			path/row and year-day of year (year-doy). Then, atmospherically 
+#			correct using LEDAPS and screen for clouds using Fmask.
+#
+###
+
+module load gdal/1.10.0
+
+line_num=''
+
+function usage {
+    cat << EOF
+    
+    usage: $0 [options] image_tarfile
+
+    This script organizes and extracts Landsat .tar.gz scene archives into
+    folders based on WRS-1 path-row and date. Next, it automates LEDAPS
+    atmospheric correction and Fmask cloud and cloud shadow detection.
+
+    Options:
+        -h  help
+        -c  cloud dilation parameter for FMASK
+        -s  shadow dilation parameter for FMASK
+		-p	cloud probability parameter for FMASK
+		-d	delete TIF files?
+        -l  do LEDAPS? 1 - yes, 0 - no (default $do_ledaps)
+		-f  do FMask? 1 - yes, 0 - no (default $do_fmask)
+		-x	do directory structure organization?	
+		-g	check for L1G images and exit if found? (default $do_L1G)
+        -u  unzip tar.gz? useful if not doing dir structure (default $do_unzip)
+        -2  Use Fmask 2.1 instead of 3.2 (default $FMASK2)
+EOF
+}
+
+function addSources {
+    # Source for commands and ancillary data
+	###
+	# Changelog:
+	#	12/08/2012 - change to Yang's compiled version
+    #   06/02/2013 - new cluster. now use module system for LEDAPS
+    #   12/19/2013 - option to use Qingsong's LC8 version of LEDAPS
+	#   08/01/2014 - use Eric Vermote's LDCM SR
+	###
+    . /usr/local/Modules/default/init/sh
+
+    module load LDCM-SR/v1.3
+    module load ledaps/20120925
+}
+
+# Custom exit failure setup
+function prepFail {
+	echo "Error: failure for id " $tarfile " at line " $line_num >> fail.log
+	echo "Directory: `pwd`" >> fail.log
+	exit 1
+}
+
+function extract {
+    if [ $do_unzip -eq 1 ]; then
+	    ### Handle tar.gz and tar.bz
+        # Get last file extension (i.e. compression type)
+	    ext=`echo $tarfile | awk -F '.' ' { print $NF } '`
+	    # Case - tar.gz
+	    if [ "$ext" == "gz" ]; then
+	    	# Extract tar.gz file
+	    	tar -xvf $tarfile
+	    elif [ "$ext" == "bz" ]; then
+	    	# Extract tar.bz file
+	    	bzip2 -cd $tarfile | tar xvf -
+	    fi
+	    # Check if success
+	    if [ `echo $?` != 0 ]; then
+	    	echo "Could not extract $tarfile"
+	    	line_num=$LINENO
+	    	prepFail
+	    else
+	    	echo "Exporting from archive completed"
+	    fi
+    else
+        tifs=`find . -name '*.TIF'`
+        mtl=`find . -name '*MTL.txt'`
+        if [ "$tifs" == "" ]; then
+            echo "Could not find TIF files. Please resubmit and unzip (-u 1)"
+        fi
+        if [ "$mtl" == "" ]; then
+            echo "Could not find MTL file. Please resubmit and unzip (-u 1)"
+        fi
+    fi
+
+	### CEHOLDEN: I don't think this does anything... test & delete
+    # Go into extracted directory, if exists
+    tardir=`echo $tarfile | tr -d .tar.gz`
+    if [ -d $tardir ]; then
+        cd $tardir
+    fi
+
+    # Find metadata file and scene id
+    # Check for MTL format
+	metadata=`find . -maxdepth 1 -name '*MTL.txt' -exec basename {} \;`
+	# Did we find anything?
+	if [ -z $metadata ]; then
+		# If not, then try for NLAPS format
+		metadata=`find . -maxdepth 1 -name 'L*_WO.txt' -exec basename {} \;`
+		# Did this find anything?
+		if [ -z $metadata ]; then
+			echo "Error: Could not find either MTL or NLAPS metadata"
+			exit 1
+		else
+			# If we did, parse into meta
+			echo $metadata
+			meta=`echo $metadata | tr -d .txt`
+			echo $meta
+		fi
+	else 
+		echo $metadata
+		meta=`echo $metadata | sed -e 's/.txt//' -e 's/_MTL//' -e 's/.met//'`
+		echo $meta
+	fi
+}
+
+function checkMTLold {
+    # Check if there's a file called "*MTLold.txt"
+    if [ -e L*MTLold.txt ]; then
+        # Rename "new" MTL file
+        mtl_new=${meta}_MTLnew.txt
+        mv $metadata $mtl_new
+        # Rename "old" MTL file
+        mtl_old=`ls L*MTLold.txt`
+        cp $mtl_old $metadata
+    # Print message
+    echo 'Using "old" MTL file. "New" MTL file moved to: ' $mtl_new
+    fi        
+}
+
+function checkL1G {
+	# Check if image is L1G and not L1T
+	l1g=`grep -a "L1G" $metadata`
+	if [[ "$l1g" == *L1G* ]]; then
+		echo "This image is L1G. Exiting"
+		echo "Image $metadata is L1G" > L1G_fail.log
+		# Removing TIF files
+		removeExtra=1
+		cleanUp
+		exit
+	fi
+	echo "This image is not L1G. Proceeding."
+}
+
+function checkLC8 {
+   
+    spacecraft=$(grep "SPACECRAFT_ID" $metadata | \
+        awk '{ print $3 }' | \
+        tr -d '"')
+    if [ "$spacecraft" == "LANDSAT_8" ]; then
+        LC8=1
+    else
+        LC8=0
+    fi
+}
+
+function get_sds() {
+    if [ -z "$1" ] || [ -z "$2" ]; then
+        echo "Missing arguments. Usage:"
+        echo "    get_sds <raster> <bands>"
+        return
+    fi
+
+    sds_names=""
+    for b in $2; do
+        sds=$(gdalinfo $1 | grep "SUBDATASET_${b}_NAME" | awk -F '=' '{ print $2 }')
+        sds_names="$sds_names $sds"
+    done
+    echo "$sds_names"
+}
+
+function gdal_extent() {
+    if [ -z "$1" ]; then 
+        echo "Missing arguments. Syntax:"
+        echo "  gdal_extent <input_raster>"
+        return
+    fi
+    EXTENT=$(gdalinfo $1 |\
+        grep "Upper Left\|Lower Right" |\
+        sed "s/Upper Left  //g;s/Lower Right //g;s/).*//g" |\
+        tr "\n" " " |\
+        sed 's/ *$//g' |\
+        tr -d "[(,]")
+    echo -n "$EXTENT"
+}
+
+function doLDCMSR {
+    set -e
+
+    echo "Running LDCM-SR located here: $(which LDCMSR)"
+
+    processldcm_bu.sh $(readlink -f ./)
+
+    echo "Converting LDCM-SR HDF output to referenced GTiff"
+
+    sr=$(ls LC8*-sr.hdf)
+    id=$(grep "LANDSAT_SCENE_ID" $metadata | awk '{ print $3 }' | tr -d '"')
+
+    # Get geotransform and projection from a TIF
+    b5=$(ls LC8*_B5.TIF)
+    ext=$(gdal_extent $b5)
+    proj=$(gdalinfo -proj4 $b5 | grep "+proj" | tr -d "'")
+
+    # Find all subdatasets and make into VRT
+    vrts=""
+    for b in $(gdalinfo $sr | grep "SUBDATASET_.*_NAME" | awk -F '_' '{ print $2 }'); do
+        temp_vrt=temp_${b}.vrt
+        sds=$(get_sds $sr $b)
+
+        gdal_translate -q -of VRT -ot Int16 $sds $temp_vrt
+        
+        gdal_edit.py -tr 30 30 $temp_vrt
+        gdal_edit.py -a_srs "$proj" $temp_vrt
+        gdal_edit.py -a_ullr $ext $temp_vrt
+
+        vrts="$vrts $temp_vrt"
+    done
+
+    gdal_merge.py -o ${id}-sr.gtif -of GTiff -ot Int16 -separate $vrts
+
+    cat <<-EOF | python - 
+from osgeo import gdal
+bands = ['Band 1 - Coastal',
+         'Band 2 - Blue',
+         'Band 3 - Green',
+         'Band 4 - Red',
+         'Band 5 - NIR',
+         'Band 6 - SWIR1',
+         'Band 7 - SWIR2',
+         'Band 9 - Cirrus',
+         'Band 10 - TIRS1',
+         'Band 11 - TIRS2',
+         'Band QA',
+         'Cloud mask']
+ds = gdal.Open('${id}-sr.gtif', gdal.GA_Update)
+hdf = gdal.Open('$sr', gdal.GA_ReadOnly)
+
+for i, (name, desc) in enumerate(hdf.GetSubDatasets()):
+    sds = gdal.Open(name, gdal.GA_ReadOnly)
+    ds.GetRasterBand(i + 1).SetMetadata(sds.GetMetadata())
+    ds.GetRasterBand(i + 1).SetDescription(bands[i])
+sds = None
+hdf = None
+ds = None
+EOF
+
+    rm temp_*.vrt
+
+    set +e
+}
+
+function doLEDAPS { 
+	ledapsDir=`which lndsr`
+	echo "lndsr location: $ledapsDir"
+
+    # Starting LEDAPS functions
+    lndpm $metadata > ledaps.log
+	if [ `echo $?` != 0 ]; then # if not success, then
+		line_num=$LINENO
+		prepFail
+	fi
+    echo "lndpm started"
+    lndcal lndcal.$meta.txt >> ledaps.log
+	if [ `echo $?` != 0 ]; then
+		line_num=$LINENO
+		prepFail
+	fi
+    echo "lndcal started"
+    lndcsm lndcsm.$meta.txt >> ledaps.log
+	if [ `echo $?` != 0 ]; then
+		line_num=$LINENO
+		prepFail
+	fi
+    echo "lndcsm started"
+    lndsr lndsr.$meta.txt >> ledaps.log
+	if [ `echo $?` != 0 ]; then
+		line_num=$LINENO
+		prepFail
+	fi
+    lndsrbm.ksh lndsr.$meta.txt >> ledaps.log
+    
+    # Changing directory back to original directory
+    echo "LEDAPs ran correctly for "$metadata
+}
+
+function doFmask {
+    # 1/23/2013:	unset license manager locations for MATLAB due to bug checking out license
+	#				for mapping toolbox
+	unset IDL_LMGRD_LICENSE_FILE
+	unset LM_LICENSE_FILE	
+
+	# Setup MATLAB execute command
+    ML="/usr/local/bin/matlab -nodisplay -nojvm -singleCompThread -r "
+    # Setup fmask commands
+	# 10/23/2012: update for Fmask 2.1sav
+    if [ "$FMASK2" -eq 0 ]; then
+        fmask="addpath('/usr3/graduate/ceholden/tools/Fmask/');"
+        fmask=$fmask"clr_pct=autoFmask(${c_dilate},${s_dilate},3,${c_prob});disp(clr_pct);"
+    else
+        fmask="addpath('/usr3/graduate/ceholden/tools/Fmask_2_1sav/');"
+	    fmask=$fmask"autoFmask_2_1sav(${c_dilate},${s_dilate},${c_prob});"
+    fi
+    fmask=$fmask"exit;"
+
+	# Print out
+	echo "Starting FMASK: "
+	echo $fmask
+
+    # Run MATLAB & fmask
+    $ML $fmask
+}
+
+function cleanUp {
+    # Removes extracted images if specified
+    if [ $removeExtra -eq 1 ]; then
+        # Remove TIF and tif
+		echo "Removing extracted single-band Landsat .TIF files"
+		find . -maxdepth 1 -name "*.TIF" -exec rm {} \;
+
+		# Remove other LEDAPS files
+		# echo "Removing non-lndsr LEDAPS files"
+		# find . -maxdepth 1 -iname "*lndcal*" -exec rm {} \;
+		# find . -maxdepth 1 -iname "*lndcsm*" -exec rm {} \;
+		# find . -maxdepth 1 -iname "*lndth*" -exec rm {} \;
+		
+		# echo "Removing other..."
+		# b6cloud file
+		# find . -maxdepth 1 -iname "*b6cloud*" -exec rm {} \;
+    fi
+}
+
+# Main function
+function doit {
+	echo "<---------------------------------------->"
+    echo "Starting process on "$tarfile
+    # Make directory for path-row
+    pathrow=`echo P${tarfile:3:3}-R${tarfile:6:3}`
+	if [ $do_dir -eq 1 ]; then
+		if [ ! -d $pathrow ]; then
+			mkdir $pathrow
+		fi
+		mv $tarfile $pathrow
+		cd $pathrow
+    
+		tarname=`echo $tarfile | awk -F '.' ' { print $1 } '`
+		if [ ! -d $tarname ]; then
+			mkdir $tarname
+		fi
+		mv $tarfile $tarname
+		cd $tarname
+	else
+		echo "Not doing directory structuring..."
+		echo "Working on: $tarfile"
+		# tarname=`echo $tarfile | tr -d .tar.gbz`
+		tarname=`find ./ -name $tarfile -exec dirname {} \;`
+		if [ ! -d $tarname ]; then
+			echo "Error: no directory named $tarname"
+			echo "Error: fail for $tarfile"
+			prepFail
+		fi
+		cd $tarname
+		tarfile=`basename $tarfile`
+	fi
+
+	# Extract files 
+	extract
+	# 09-06-2012 ceholden: edit
+	#   Add in check for "new" MTL file format
+	#   Current version of LEDAPS doesn't support new tags
+	# 02/03/2013 ceholden: edit
+	#	New versions of Fmask & LEDAPS support new MTL file
+	#	Commenting out this step	
+	# checkMTLold
+	# CEHOLDEN (11/23/2012): Check if image is L1G - quit if true
+	if [ $do_L1G -eq 1 ]; then
+		checkL1G
+	fi
+	
+	# CEHOLDEN (10/23/2012): Fmask_2_1sav gets confused if LEDAPS is done first
+	# Perform FMASK
+	if [ $do_fmask -eq 1 ]; then
+		doFmask
+	fi
+	# Perform LEDAPS
+	if [ $do_ledaps -eq 1 ]; then
+        checkLC8
+        if [ "$LC8" -ne 1 ]; then
+    		echo "Running SR correction on Landsat 4-7 - LEDAPS"
+            doLEDAPS
+        else
+            echo "Running SR correction on Landsat 8 - LDCMSR"
+            doLDCMSR
+        fi
+	fi
+	# Clean up files?
+	cleanUp
+}
+
+tarfile=''
+metadata=''
+removeExtra=0
+c_dilate=3
+s_dilate=3
+c_prob=22.5
+do_ledaps=1
+do_fmask=1
+do_dir=1
+do_L1G=0
+do_unzip=1
+LC8=0
+FMASK2=0
+
+# Parse arguments
+while getopts "hc:s:p:dl:f:x:g:u:2:" opt
+do
+        case $opt in
+        h)
+            usage
+            exit 0
+            ;;
+        c)
+            c_dilate=$OPTARG
+            ;;
+        s)
+            s_dilate=$OPTARG
+            ;;
+		p)
+			c_prob=$OPTARG
+			;;
+        d)
+            removeExtra=1
+            ;;
+		l)
+			do_ledaps=$OPTARG
+			;;
+		f)
+			do_fmask=$OPTARG
+			;;
+		x)
+			do_dir=$OPTARG
+			;;
+		g)
+			do_L1G=$OPTARG
+			;;
+        u)
+            do_unzip=$OPTARG
+            ;;
+        2)
+            FMASK2=$OPTARG
+            ;;
+        esac
+done
+
+shift $(($OPTIND - 1 ))
+
+# Parse positional entry
+if [ -n "$1" ]; then
+    tarfile=$1
+else
+	echo "Error: no tar.gz input"
+	line_num=$LINENO
+	prepFail
+fi
+
+echo 'Image: ' $tarfile
+echo "Parameters: "
+echo "Cloud dilate: " $c_dilate
+echo "Shadow dilate: " $s_dilate
+echo "Cloud probability: " $c_prob
+echo "Remove TIF: " $removeExtra
+echo "Do LEDAPS?: " $do_ledaps
+echo "Do Fmask?: " $do_fmask
+echo "Check for L1G?: " $do_L1G
+echo "Unzip tarfile?: " $do_unzip
+echo "Use FMASK 2.1: " $FMASK2
+
+# Add sources for LEDAPS
+addSources
+# Run main method
+doit
---
+++ landsat/prep/landsatPrepSubmit.sh
@@ -0,0 +1,478 @@
+#!/bin/bash
+
+###
+#
+# Name:     landsatPrepSubmit.sh
+# Version:  3.0
+# Date:     10/27/2012
+#
+# Author:   Chris Holden (ceholden@bu.edu)
+#           Modified from code for MCD12Q2 processing from Koen Hufkens
+#
+# Purpose:  Finds Landsat tar.gz and sets up qsub jobs to run
+#           landsatPrepQsub.sh on all tar.gz, thereby pre-processing them.
+#           Takes input of maximum simulataneous qsub jobs and directory
+#           containing image archives.
+#
+# Updates:
+#   3.0:    Added option to skip either LEDAPS or FMASK.
+#   4.0:    Use Eric Vermote's LDCM SR correction
+#
+###
+
+###
+# Variables
+###
+
+# User-id
+USERID=`id -un`
+# Job basename
+JOBNAME=`id -gn`
+# Maximum jobs to submit at once
+NJobsMax=2
+# Jobs submitted
+NJobsSub=''
+# Jobs running/queue
+NJobsRun=''
+# Jobs running
+NJobsRunning=''
+# Jobs waiting
+NJobsWaiting=''
+# Sleep time between checks (seconds)
+SleepTime=60
+
+# Defaults
+removeExtra=0
+cloud_dilate=3
+shadow_dilate=3
+c_prob=22.5
+
+# Text file containing output of `find ...` for tar.gz
+tarlist='landsat_scenes.txt'
+# Number of tar.gz files found in $tarlist
+Nscene=''
+# Directory given at startup
+startdir=''
+# Directory for scripts
+scriptdir='qsub_scripts'
+# Current scene and script
+scene=''
+scriptfile=''
+createdScripts=0
+
+# Boolean switches for LEDAPS/Fmask
+do_ledaps=1
+do_fmask=1
+# Organize by directory?
+do_dir=1
+# Check for L1G images?
+do_L1G=0
+# Send email for each job?
+doMail=1
+# Unzip tar.gz?
+do_unzip=1
+# Use Fmask 2.1
+FMASK2=0
+
+###
+# Usage
+###
+function usage {
+    cat << EOF
+    
+    usage: $0 [options] image_directory
+
+    Author: Chris Holden (ceholden@bu.edu)
+
+    Purpose:
+    This script generates and manages the submissions of Landsat
+    pre-processing (LEDAPS/Fmask) jobs to the Sun Grid Engine on BU's
+    Katana cluster. 
+
+    Note: requires script "landsatPrepQsub.sh" to also be in your path
+
+    Options:
+        -h  help
+        -m  maximum SGE jobs at one time (default ${NJobsMax})
+        -n  base for job names (default - ${JOBNAME}; cannot begin with a #)
+        -w  wait between qstat checks (default ${SleepTime}s)
+        -d  delete TIF files?
+        -c  cloud dilation parameter for FMASK (default ${cloud_dilate})
+        -s  shadow dilation parameter for FMASK (default ${shadow_dilate})
+        -p  cloud probability parameter for FMASK (default ${c_prob})
+        -l  do LEDAPS? 1 - yes, 0 - no (default $do_ledaps)
+        -f  do FMask? 1 - yes, 0 - no (default $do_fmask)
+        -x  do directory structure organization? (default $do_dir)
+            0 - no, just find tar.gz and move to their locations
+            1 - yes, creates directory structure
+        -g  check for L1G images and exit if found? (default $do_L1G)
+        -e  send email? (default $doMail)
+        -u  do unzipping? useful if data already extracted (default $do_unzip)
+        -2  Use Fmask 2.1 (default $FMASK2)
+
+    Examples:
+        Run LEDAPS and Fmask (3.2) using 10 jobs maximum and use custom Fmask 
+        options on the folder "images":
+        > landsatPrepSubmit.sh -m 10 -n myjob -w 60 -d -c 5 -s 4 -p 12.5 images/
+
+        Run ONLY Fmask (3.2) on the files processed above, but this time with a 
+        different Fmask cloud probability. Note the "-x" option:
+        > landsatPrepSubmit.sh -m 10 -n myjob -c 5 -s 4 -p 22.5 images/P012-R031
+
+        Run ONLY Fmask (3.2) on the files processed above, but with a different
+        set of Fmask dilation values. Note that because we did not ask for the 
+        TIF files to be deleted in the last run, they still exist and we will 
+        not extract them ("-u 0").
+        > landsatPrepSubmit.sh -m 10 -n myjob -c 3 -s 3 -p 22.5 images/P012-R031
+
+EOF
+}
+
+###
+# Search in current directory for *.tar.gz. Write to file
+###
+function find_tars {
+    # Remove old list if exists
+    if [ -f $tarlist ]; then
+        rm $tarlist
+        # Check exit status
+        if [ `echo $?` != 0 ]; then
+            echo "Error: could not delete old list of tar.gz"
+            exit 1
+        fi
+    fi
+
+    if [ "$do_dir" -eq "1" ]; then
+        # Check if we can find Landsat archives
+        Nscene=`find . -maxdepth 1 -name 'L*.tar.*z' | wc -l`
+        # Check if we found anything
+        if [ ! "$Nscene" -gt "0" ]; then
+            echo "Error: no *.tar.gz found in $startdir"
+            exit 1
+        else
+            echo "Found ${Nscene} Landsat archives"
+        fi
+    
+        # Find all tar.gz in current directory and write to file
+        find . -noleaf -maxdepth 1 -name 'L*.tar.*z' -exec basename {} \; > $tarlist
+        # Ensure we could write out list
+        if [ `echo $?` != 0 ]; then
+            echo "Error: could not write list of *.tar.gz to file"
+            exit 1
+        fi
+    else
+        echo "Not doing directory structure..."
+        # Check if we can find Landsat archives
+        echo `pwd`
+        Nscene=`find L* -name 'L*.tar.*z' | wc -l`
+        echo $Nscene
+        # Check if we found anything
+        if [ ! "$Nscene" -gt "0" ]; then
+            echo "Error no *.tar.gz found in $startdir"
+            exit 1
+        else
+            echo "Found ${Nscene} Landsat archives"
+        fi
+
+        # Find all tar.gz in current directory and write to file
+        find . -name 'L*.tar.*z' -exec basename {} \; > $tarlist
+        # Ensure we could write out list
+        #if [ `echo $?` != 0 ]; then
+        #    echo "Error: could not write list of *.tar.gz to file"
+        #    exit 1
+        #fi
+    fi
+}
+
+
+###
+# Write job to script file
+# Depends on: $scene, $scneid, $scriptfile, $scriptdir, $startdir
+###
+function write_job {
+    # If old $scriptdir exists, delete it
+    if [ -d $scriptdir -a "$createdScripts" -eq 0 ]; then
+        rm -r $scriptdir
+    fi
+    if [ "$createdScripts" -eq 0 ]; then
+        mkdir $scriptdir
+    fi
+
+    # Begin echo statmenets for qsub job file
+    echo "#!/bin/sh" > $scriptdir/$scriptfile
+    # SGE options
+    echo "#$ -V" >> $scriptdir/$scriptfile
+    echo "#$ -l h_rt=2:00:00" >> $scriptdir/$scriptfile
+    echo "#$ -N $JOBNAME.$sceneid" >> $scriptdir/$scriptfile
+    echo "" >> $scriptdir/$scriptfile
+    # Make sure we change directory to $startdir
+    echo "cd $startdir" >> $scriptdir/$scriptfile
+    echo "" >> $scriptdir/$scriptfile
+    
+    # Command
+    command="landsatPrepQsub.sh"
+    # Options
+    # Remove TIF?
+    if [ $removeExtra == 1 ]; then
+        command=`echo "$command -d"`
+    fi
+    # Add in dilation & tar.gz name
+    command=`echo "$command -c $cloud_dilate -s $shadow_dilate"`
+    # Add in cloud probability
+    command=`echo "$command -p $c_prob"`
+    # Add in do_ledaps, do_fmask
+    command=`echo "$command -l $do_ledaps -f $do_fmask -x $do_dir"`
+    command=`echo "$command -g $do_L1G -u $do_unzip -2 $FMASK2 $scene"`
+    echo $command >> $scriptdir/$scriptfile
+    echo "" >> $scriptdir/$scriptfile
+    # Wait
+    echo "wait" >> $scriptdir/$scriptfile
+    
+    # Mod permissions
+    chmod a+rwx $scriptdir/$scriptfile
+}
+
+###
+# Create qsub job files
+# Some code modified from Koen's script
+###
+function create_jobs {
+    ### Initialize text files for each tar.gz
+    # Case found by Josh Gray when n=1
+    if [ $Nscene -eq 1 ]; then
+        i=1
+        # Get archive name
+        scene=`awk "NR==$i" $tarlist`
+        # Remove trailing .tar.gz
+        sceneid=`echo $scene | awk 'BEGIN { FS = "." }; { print $1 }'`
+        # Script file name
+        scriptfile=`echo $sceneid.$i.sh`
+        # Write out scripts
+        write_job
+        createdScripts=1
+    else
+        for i in `seq 1 $Nscene`;
+        do
+            # Get archive name
+            scene=`awk "NR==$i" $tarlist`
+            # Remove trailing .tar.gz
+            sceneid=`echo $scene | awk 'BEGIN { FS = "." }; { print $1 }'`
+            # Script file name
+            scriptfile=`echo $sceneid.$i.sh`
+            # Write out scripts
+            write_job
+            createdScripts=1
+        done
+    fi
+
+    echo "Wrote jobs out..."
+
+    ### Submit jobs
+    # Set counters
+    NJobsRun=`qstat -u $USERID | grep $JOBNAME | awk '{print $5}' | wc -l`
+    # NOTE: NJobsSub set to 0 for reporting
+    #       scene_num=(NJobsSub + 1) used for line numbers and scriptID
+    NJobsSub=0
+   
+    echo "Jobs running: $NJobsRun"
+
+    # While we have jobs to submit
+    while [ "$NJobsSub" -lt $Nscene ];
+    do
+        # Get jobs in SGE queue
+        NJobsRun=`qstat -u $USERID | grep $JOBNAME | awk '{print $5}' | wc -l`
+        # Get jobs running in SGE queue
+        NJobsRunning=`qstat -u $USERID | grep $JOBNAME | awk '{print $5}' | 
+            grep r | wc -l`
+        NJobsWaiting=`qstat -u $USERID | grep $JOBNAME | awk '{print $5}' | 
+            grep qw | wc -l`
+
+        # Check if NJobsRun < NJobsMax
+        if [ "$NJobsRun" -lt "$NJobsMax" ]; then 
+            # If so, submit a job
+            scene_num=`expr $NJobsSub + 1`
+            scene=`awk "NR==$scene_num" $tarlist`
+            sceneid=`echo $scene | awk 'BEGIN { FS = "." }; { print $1 }'`
+            scriptfile=`echo $sceneid.$scene_num.sh`
+            
+            # Submit
+            echo "<------------------------->"
+            if [ $doMail == 0 ]; then
+                qsub -m n $scriptdir/$scriptfile
+            else
+                qsub $scriptdir/$scriptfile
+            fi
+            # Message
+            echo "Submitted job #${scene_num}"
+
+            # Change counter
+            NJobsSub=`expr $NJobsSub + 1`
+            # Sleep a little to allow SGE to register job
+            sleep 2
+        else
+            # We have max jobs running; update and wait
+            sleep $SleepTime
+        fi
+        
+        # Print out some status
+        echo "<------------------------->"
+        echo "Jobs submitted: " $NJobsSub
+        echo "Jobs on SGE: " $NJobsRun
+        echo "Jobs running: " $NJobsRunning
+        echo "Jobs waiting: " $NJobsWaiting
+    done
+
+    # Done!
+    echo "All jobs submitted to SGE. Exiting."
+    exit 0
+}
+
+##### STARTS HERE
+# Parse arguments
+while getopts "hdc:s:p:m:w:n:l:f:x:g:e:u:2:" opt
+do
+    case $opt in
+    h)
+        usage
+        exit 0 
+        ;;
+    m)
+        NJobsMax=$OPTARG
+        ;;
+    w)
+        SleepTime=$OPTARG
+        ;;
+    n)
+        JOBNAME=$OPTARG
+        ;;
+    c)
+        cloud_dilate=$OPTARG
+        ;;
+    s)
+        shadow_dilate=$OPTARG
+        ;;
+    p)
+        c_prob=$OPTARG
+        ;;
+    d)
+        removeExtra=1
+        ;;
+    l)
+        do_ledaps=$OPTARG
+        ;;
+    f)
+        do_fmask=$OPTARG
+        ;;
+    x)
+        do_dir=$OPTARG
+        ;;
+    g)
+        do_L1G=$OPTARG
+        ;;
+    e)
+        doMail=$OPTARG
+        ;;
+    u)
+        do_unzip=$OPTARG
+        ;;
+    2)
+        FMASK2=$OPTARG
+        ;;
+    ?)
+        echo "Error: unknown option -$opt"
+        exit
+    esac
+done
+
+# Shift
+shift $(($OPTIND - 1 ))
+
+# Check for image archive directory at $1
+if [ -n "$1" ]; then
+    startdir=$1
+else
+    startdir=`pwd`
+fi
+# Get full path of startdir
+startdir=`readlink -f $startdir`
+# Check if startdir exists
+if [ ! -d $startdir ]; then
+    echo "Error: directory '$startdir' does not exist"
+    exit 1
+fi
+
+# Check that JOBNAME doesn't start with a number
+if [[ $JOBNAME == [0-9]* ]]; then 
+    echo "Error: job basename cannot start with a number"
+    exit 1
+fi
+
+# Check that dilation parameters are integers
+if ! [[ "$cloud_dilate" =~ ^[0-9]+$ ]]; then
+    echo "Error: cloud dilation parameter must be an integer"
+    exit 1
+fi
+if ! [[ "$shadow_dilate" =~ ^[0-9]+$ ]]; then
+    echo "Error: shadow dilation parameter must be an integer"
+    exit 1
+fi
+
+# Check input for do_ledaps / do_fmask
+if [[ $do_ledaps -ne 1 && $do_ledaps -ne 0 ]]; then
+    echo "Error: -l option must be boolean 0 or 1"
+    exit 1
+fi
+if [[ $do_fmask -ne 1 && $do_fmask -ne 0 ]]; then
+    echo "Error: -f option must be boolean 0 or 1"
+    exit 1
+fi
+
+if [[ $do_dir -ne 1 && $do_dir -ne 0 ]]; then
+    echo "Error: -x option must be boolean 0 or 1"
+    exit 1
+fi
+
+if [[ $do_L1G -ne 1 && $do_L1G -ne 0 ]]; then
+    echo "Error: -g option must be boolean 0 or 1"
+    exit 1
+fi
+if [[ $doMail -ne 1 && $doMail -ne 0 ]]; then
+    echo "Error: -e option must be boolean 0 or 1"
+    exit 1
+fi
+if [[ $do_unzip -ne 1 && $do_unzip -ne 0 ]]; then
+    echo "Error: -e option must be boolean 0 or 1"
+    exit 1
+fi
+if [[ $FMASK2 -ne 1 && $FMASK2 -ne 0 ]]; then
+    echo "Error: -2 option must be boolean 0 or 1"
+    exit 1
+fi
+
+# Echo starting parameters
+echo "Scene directory: " $startdir
+echo "Maximum jobs: " $NJobsMax
+echo "Job basename: " $JOBNAME
+echo "Time between qstat checks: " $SleepTime
+echo "Cloud dilation: " $cloud_dilate
+echo "Shadow dilation: " $shadow_dilate
+echo "Cloud probability: " $c_prob
+echo "Remove unneccessary files?: " $removeExtra
+echo "Do LEDAPS?: " $do_ledaps
+echo "Do Fmask?: " $do_fmask
+echo "Check for L1G?: " $do_L1G
+echo "Send email?: " $doMail
+echo "Do unzip?: " $do_unzip
+echo "Use FMask 2.1sav: " $FMASK2
+
+# Check for companion script in PATH
+command -v landsatPrepQsub.sh >/dev/null 2>&1 || { 
+    echo >&2 "Error: 'landsatPrepQsub.sh' not found. Check your PATH"
+    exit 1
+}
+
+# Start process
+cd $startdir
+find_tars
+# Write jobs and submit them
+create_jobs
