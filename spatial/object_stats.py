b7425fc300ca34182feb8c5bd69c4bde795dc080
Chris Holden <ceholden@gmail.com>
2015-07-14 20:08:16 -0400

    Make object stats work on non-sequential seg images; PEP8

--- spatial/object_stats.py
+++ spatial/object_stats.py
@@ -1,158 +1,183 @@
 #!/usr/bin/env python
 # /*****************************************************************************
 # * Name:       object_stats.py
-# * Author:     Chris Holden (ceholden@bu.edu)
+# * Author:     Chris Holden (ceholden@gmail.com)
 # * Version:    1.0
-# * Purpose:    To read an image and segmentation image using GDAL and to 
-# *             calculate statistics for each segment using the image.  
+# * Purpose:    To read an image and segmentation image using GDAL and to
+# *             calculate statistics for each segment using the image.
 # * Methods:    Use GDAL (gdal.org) to read the images and import into numpy
 # *             arrays. Next, use find_objects to get i, j coordinates in image
 # *             of pixels in i=1:n segments. Finally, use ndimage library
 # *             to calculate statistic for given segment.
 # *
 # *****************************************************************************\
-import sys
-import os
+from __future__ import division, print_function
 import argparse
+import logging
+import sys
 
-import numpy
+import numpy as np
 from scipy import ndimage
 
-try:
-    from osgeo import gdal
-    from osgeo.gdalnumeric import *
-except ImportError:
-    import gdal
-    from gdalnumeric import *
+from osgeo import gdal, gdal_array
+
+__version__ = '1.0.0'
+
+gdal.AllRegister()
+gdal.UseExceptions()
+
+logging.basicConfig(format='%(asctime)s.%(levelname)s: %(message)s',
+                    level=logging.INFO,
+                    datefmt='%H:%M:%S')
+logger = logging.getLogger(__name__)
+
+STATISTICS = ['mean', 'var', 'num', 'max', 'min', 'sum']
+
 
 def objstats(args):
-    # Register all drivers
-    gdal.AllRegister()
-
-    # Open up images as datasets
+    # Open and read from image and segmentation
     try:
-        imgDs = gdal.Open(args.image, gdal.GA_ReadOnly)
+        img_ds = gdal.Open(args.image, gdal.GA_ReadOnly)
     except:
-        imgDs is None
-    if imgDs is None:
-        print 'Error: Could not open ' + args.image
+        logger.error('Could not open image: {}'.format(i=args.image))
         sys.exit(1)
-        
+
     try:
-        segDs = gdal.Open(args.segment, gdal.GA_ReadOnly)
+        seg_ds = gdal.Open(args.segment, gdal.GA_ReadOnly)
     except:
-        segDs is None
-    if segDs is None:
-        print 'Error: Could not open ' + args.segment
+        logger.error('Could not open segmentation image: {}'.format(
+            i=args.segment))
         sys.exit(1)
-    
-    # Get dimensions
-    cols = imgDs.RasterXSize
-    rows=imgDs.RasterYSize
-    bands=imgDs.RasterCount
 
-    # Init img as numpy matrix
-    img=zeros([rows, cols])
+    cols, rows = img_ds.RasterXSize, img_ds.RasterYSize
+    bands = img_ds.RasterCount
 
-    # Init seg as numpy matrix
-    seg=zeros([rows, cols])
-    # Get segment band from dataset and read in as numpy matrix
-    segBand = segDs.GetRasterBand(1)
-    seg = segBand.ReadAsArray(0, 0, cols, rows).astype(
-        gdal.GetDataTypeName(segBand.DataType))
+    seg_band = seg_ds.GetRasterBand(1)
+    seg = seg_band.ReadAsArray(0, 0, cols, rows).astype(
+        gdal_array.GDALTypeCodeToNumericTypeCode(seg_band.DataType))
+    logger.debug('Read segmentation image into memory')
+
     # Get list of unique segments
-    useg = unique(seg)
-    
-    # Hack - if calc is num, do only for 1 band
+    useg = np.unique(seg)
+    sequential = False
+    if np.array_equal(useg, np.arange(useg.min(), useg.max(), 1)):
+        logger.debug('Segmentation image is sequential. Can use fast method')
+        sequential = True
+    else:
+        logger.debug(
+            'Segmentation image is not sequential. Processing will be slower')
+
+    # If calc is num, do only for 1 band
     if args.stat == 'num':
         bands = 1
+
     # Create output driver
     driver = gdal.GetDriverByName(args.format)
-    outDs = driver.Create(args.output, cols, rows, bands, gdal.GDT_Float32)
-    # Create output same as image
-    out = zeros_like(img)
-    
+    out_ds = driver.Create(args.output, cols, rows, bands, gdal.GDT_Float32)
+
     # Loop through image  bands
-    print 'About to loop through ' + str(bands) + ' bands'
+    out_2d = np.empty_like(seg, dtype=np.float32)
     for b in range(bands):
-        print 'Band ' + str(b + 1)
-        # Get each band and read into numpy matrix
-        imgBand = imgDs.GetRasterBand(b+1)
-        NDV = imgBand.GetNoDataValue()
-        img = imgBand.ReadAsArray(0, 0, cols, rows).astype(
-            gdal.GetDataTypeName(imgBand.DataType))
-        
+        logger.info('Processing band: {i}'.format(i=b + 1))
+        img_band = img_ds.GetRasterBand(b + 1)
+        ndv = img_band.GetNoDataValue()
+        img = img_band.ReadAsArray().astype(
+            gdal_array.GDALTypeCodeToNumericTypeCode(img_band.DataType))
+        logger.debug('Read image band {b} into memory'.format(b=b + 1))
+
+        # Mask out segment values where img == NoDataValue
+        ndv_seg = seg.copy()
+        # if ndv is not None:
+        #     ndv_seg[img == ndv] = 0
+
         if args.stat == 'mean':
             # Mean for all regions
-            out = ndimage.mean(img, seg, useg)
-            out = out[seg - seg.min()]
+            logger.debug('Calculating mean')
+            out = ndimage.mean(img, ndv_seg, useg)
         elif args.stat == 'var':
             # Variance for all regions
-            out = ndimage.variance(img, seg, useg)
-            out = out[seg - seg.min()]
+            out = ndimage.variance(img, ndv_seg, useg)
         elif args.stat == 'num':
             # Number of pixels in segment
-            count = ones_like(seg)
-            out = ndimage.sum(count, seg, useg)
-            out = out[seg - seg.min()]
+            count = np.ones_like(ndv_seg)
+            out = ndimage.sum(count, ndv_seg, useg)
         elif args.stat == 'sum':
             # Sum of each band in segments
-            out = ndimage.sum(img, seg, useg)
-            out = out[seg - seg.min()]
+            out = ndimage.sum(img, ndv_seg, useg)
         elif args.stat == 'min':
             # Minimum pixel in each segment
-            out = ndimage.minimum(img, seg, useg)
-            out = out[seg - seg.min()]
+            out = ndimage.minimum(img, ndv_seg, useg)
         elif args.stat == 'max':
             # Maximum pixel in each segment
-            out = ndimage.maximum(img, seg, useg)
-            out = out[seg - seg.min()]
-            
+            out = ndimage.maximum(img, ndv_seg, useg)
+        logger.debug('Computed statistic for all segment IDs')
+
+        # Transform to 2D
+        if sequential:
+            out = out[ndv_seg - ndv_seg.min()]
+        else:
+            for i, u in np.ndenumerate(useg):
+                r, c = np.where(ndv_seg == u)
+                out_2d[r, c] = out[i]
+        logger.debug('Applied statistic to entire image')
+
         # Write out the data
-        outBand = outDs.GetRasterBand(b+1)
-        if NDV is not None:
-            outBand.SetNoDataValue(NDV)
-        outBand.WriteArray(out, 0, 0)
-        outBand.FlushCache()
-        
-    outDs.SetGeoTransform(imgDs.GetGeoTransform())
-    outDs.SetProjection(imgDs.GetProjection())
-    
-    imgDs = None
-    segDs = None
-    outDs = None    
-    
-    
+        out_band = out_ds.GetRasterBand(b + 1)
+        if ndv is not None:
+            out_band.SetNoDataValue(ndv)
+        logger.debug('Writing object statistic for band {b}'.format(b=b + 1))
+        out_band.WriteArray(out_2d, 0, 0)
+        out_band.FlushCache()
+        logger.debug('Wrote out object statistic for band {b}'.format(b=b + 1))
+
+    out_ds.SetGeoTransform(img_ds.GetGeoTransform())
+    out_ds.SetProjection(img_ds.GetProjection())
+
+    img_ds = None
+    seg_ds = None
+    out_ds = None
+    logger.info('Completed object statistic calculation')
+
+
 # Main program
 def main():
     desc = "Calculate a given statistic for pixels in each segment"
-    parser = argparse.ArgumentParser(prog='window_stats.py', description=desc)
-    
-    parser.add_argument('-s', action='store', dest='stat', type=str,
-        help='statistic to calculate (mean, var, num, max, min, sum)',
-        default='mean')
-    
-    parser.add_argument('--f', dest='format', default='GTiff',
-        help='GDAL format for output file (default "GTiff")')
+    parser = argparse.ArgumentParser(prog='object_stats.py', description=desc)
 
-    parser.add_argument('image', action='store', type=str, 
+    parser.add_argument('--version', action='version',
+                        version='%(prog)s v{v}'.format(v=__version__))
+    parser.add_argument('--verbose', '-v', help="increase output verbosity",
+                        action="store_true")
+    parser.add_argument(
+        '-s', action='store', dest='stat', type=str,
+        help='statistic to calculate ({c})'.format(c=', '.join(STATISTICS)),
+        default='mean')
+    parser.add_argument(
+        '-of', dest='format', default='GTiff',
+        help='GDAL format for output file (default "GTiff")')
+    parser.add_argument(
+        'image', action='store', type=str,
         help='input image raster file')
-    parser.add_argument('segment', action='store', type=str,
+    parser.add_argument(
+        'segment', action='store', type=str,
         help='input segment raster file')
-    parser.add_argument('output', action='store', type=str, 
+    parser.add_argument(
+        'output', action='store', type=str,
         help='output raster file')
 
     args = parser.parse_args()
 
-    possible = ['mean', 'var', 'num', 'max', 'min', 'sum']
-    if (args.stat in possible) is False:
-        print 'Error: statistic incorrect or not available.'
-        print parser.print_help()
+    if args.verbose:
+        logger.setLevel(logging.DEBUG)
+
+    if args.stat not in STATISTICS:
+        logger.error('Statistic {s} is incorrect or not available.'.format(
+            s=args.stat))
+        parser.print_help()
         sys.exit(1)
     else:
         objstats(args)
 
-    sys.exit(1)
-    
+
 if __name__ == '__main__':
-     main()
+    main()
