cce32ab3a2b2a9d42df3e4067f5fde3987332749
Chris Holden <ceholden@gmail.com>
2016-12-14 11:35:52 -0500

    Try 3x 4K res at native on subset wo annotate

--- viz/clip_and_stretch.sh
+++ viz/clip_and_stretch.sh
@@ -29,10 +29,19 @@ OUT=$1
 
 
 # STYLE
-BOUNDS="209565.0 4671855.0 382365.0 4704255.0"
 
-FORMAT=JPEG
-EXT=jpg
+ANNOTATE=0
+
+# 3x 1920x1080
+# BOUNDS="209565.0 4671855.0 382365.0 4704255.0"
+
+# 3x 3840x2160
+BOUNDS="228495.0 4638585.0 574095.0 4703385.0"
+
+#FORMAT=JPEG
+#EXT=jpg
+FORMAT=GTiff
+EXT=tif
 
 PTSIZE=100
 GRAVITY=South
@@ -43,8 +52,8 @@ ROOT=/projectnb/landsat/projects/Massachusetts/p012r031/images
 OUT=$OUT/$FORMAT/
 TMP=${OUT}/TEMP
 CLIPPED=$OUT/../clipped
-B543=$OUT/B543/out
-B432=$OUT/B432/out
+B543=$OUT/B543
+B432=$OUT/B432
 
 mkdir -p $CLIPPED
 mkdir -p $B543
@@ -55,17 +64,21 @@ sort_date() { date -d "$1-01-01 +$2 days -1 day" "+%Y%m%d"; }
 
 clip_stretch() {
     echo "Working on: $2"
-    
+
     img=$1
     name=$2
+    prefix="$3"
 
     clipped=$CLIPPED/$name.gtif
 
     if [ ! -f $clipped ]; then
-        rio clip --bounds $BOUNDS $img $clipped
+    #    rio clip --bounds $BOUNDS $img $clipped
+        gdal_translate \
+            -srcwin 1011 1147 11520 2160 \
+            $img $clipped
     fi
-   
-    if [ ! -f $B543/$name.${EXT} ]; then 
+
+    if [ ! -f $B543/$name.${EXT} ]; then
         $STRETCH \
             -b 5 -mm 500 3500 \
             -b 4 -mm 0 6000 \
@@ -73,7 +86,7 @@ clip_stretch() {
             -f $FORMAT -ot uint8 \
             --ndv -9999 --ndv 20000 \
             --co "QUALITY=95" \
-            $clipped $B543/$name.${EXT} linear
+            $clipped $B543/${prefix}$name.${EXT} linear
     fi
 
     if [ ! -f $B432/$name.${EXT} ]; then
@@ -84,7 +97,7 @@ clip_stretch() {
             -f $FORMAT -ot uint8 \
             --ndv -9999 --ndv 20000 \
             --co "QUALITY=95" \
-            $clipped $B432/$name.${EXT} linear
+            $clipped $B432/${prefix}$name.${EXT} linear
     fi
 }
 
@@ -110,7 +123,7 @@ annotate() {
     for d in $B543 $B432; do
         img=$d/${name}.${EXT}
         out=$d/../${_sort_date}_${name}.${EXT}
-        
+
         convert $img \
             -pointsize $PTSIZE \
             -gravity $GRAVITY \
@@ -122,28 +135,38 @@ annotate() {
 
 for img in $(find $ROOT/LC8* -name 'L*_all'); do
     name=$(echo $(basename $img) | awk -F '_' '{ print $1 }')
-    clip_stretch $img $name
-    annotate $name
-done
-
-for d in $B543 $B432; do
-    for img in $d/*.${EXT}; do
-        name=$(basename $img .${EXT})
+    if [ "$ANNOTATE" == "1" ]; then
+        clip_stretch $img $name
+        annotate $name
+    else
         year=${name:9:4}
         doy=${name:13:3}
-        _date=$(pretty_date $year $doy)
         _sort_date=$(sort_date $year $doy)
 
-        out=$d/../${_sort_date}_${name}.${EXT}
-        
-        convert $img \
-            -pointsize $PTSIZE \
-            -gravity $GRAVITY \
-            -stroke '#000C' -strokewidth 2 \
-            -stroke  none   -fill white \
-            -annotate 0 "$_date" \
-            $out
-    done
+        clip_stretch $img $name ${_sort_date}_
+    fi
 done
+
+if [ "$ANNOTATE" == "1" ]; then
+    for d in $B543 $B432; do
+        for img in $d/*.${EXT}; do
+            name=$(basename $img .${EXT})
+            year=${name:9:4}
+            doy=${name:13:3}
+            _date=$(pretty_date $year $doy)
+            _sort_date=$(sort_date $year $doy)
+
+            out=$d/../${_sort_date}_${name}.${EXT}
+
+            convert $img \
+                -pointsize $PTSIZE \
+                -gravity $GRAVITY \
+                -stroke '#000C' -strokewidth 2 \
+                -stroke  none   -fill white \
+                -annotate 0 "$_date" \
+                $out
+        done
+    done
+fi
 
 echo "Complete!"
